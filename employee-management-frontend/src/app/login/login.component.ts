94d7fc974df92f4f50b88e8ccd9c232b2f2365e0
Kirandeep Gantasala <kirandeepgantasala456@gmail.com>
2026-05-17 00:38:13 +0530

    Changed temp.ts to LoginRequest.ts

--- employee-management-frontend/src/app/login/login.component.ts
+++ employee-management-frontend/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { LoginRequest } from '../model/temp';
+import { LoginRequest } from '../model/LoginRequest';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
